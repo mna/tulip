ff330b267f63e766ad7884d718c0167d9e53b5d1
Martin Angers <martin.n.angers@gmail.com>
2020-10-09 14:49:53 -0400

    Try to create working server/client testing helper

--- test/main.lua
+++ test/main.lua
@@ -1,6 +1,7 @@
 local lu = require 'luaunit'
 
 TestMigrate = require 'test.migrate'
+TestWeb = require 'test.web'
 TestXstring = require 'test.xstring'
 
 os.exit(lu.LuaUnit.run())
---
+++ test/web.lua
@@ -0,0 +1,70 @@
+local cqueues = require 'cqueues'
+local headers = require 'http.headers'
+local inspect = require 'inspect'
+local posix = require 'posix'
+local request = require 'http.request'
+local server = require 'http.server'
+local stdio = require 'posix.stdio'
+
+local M = {}
+
+function M.test_web_server()
+  --[[
+      print('>>>>> client read')
+      local port = con:read('*l')
+      print('>>>>> client done', port)
+      cqueues.sleep(0.5)
+      local req = request.new_from_uri(string.format('http://127.0.0.1:%s/', port))
+      print('>>>>> client request created', req:to_uri())
+      local hdrs, res = req:go(10)
+      print('>>>> client got', hdrs, res)
+      --print(hdrs:get(':status'))
+      --print(res:get_body_as_string(10))
+  ]]--
+
+
+  local pipe = assert(posix.popen(function()
+    local srv = server.listen{
+      host = '127.0.0.1',
+      port = 0,
+      reuseaddr = true,
+      reuseport = true,
+      onstream = function(_, stm)
+        print('>>>>> server request received')
+        local hdrs = headers.new()
+        hdrs:upsert(':status', '200')
+        print('>>>>> server headers set')
+        local ok, msg = pcall(stm.write_headers, stm, hdrs, false)
+        print('>>>>> server write headers done', ok, msg)
+        stm:write_body_from_string('allo', 10)
+      end,
+    }
+    assert(srv:listen())
+    local _, _, port = srv:localname()
+    io.write(port .. '\n')
+    --print('>>>>> server will start')
+    assert(srv:loop())
+    --print('>>>>> server stopped')
+    return 0
+  end, 'r'))
+
+  local pfd = assert(stdio.fdopen(pipe.fd, 'r'))
+  local port = pfd:read('l')
+  io.write(string.format('>>> got from spwan: %q\n' , port))
+
+  local cq = cqueues.new()
+  cq:wrap(function()
+    local req = request.new_from_uri(string.format('http://127.0.0.1:%s/', port))
+    --print('>>>>> client request created', req:to_uri())
+    local hdrs, res = req:go(10)
+    print('>>>> client got', hdrs, res)
+    for ln in pfd:lines() do
+        print('>>> from server:', ln)
+    end
+    --print(hdrs:get(':status'))
+    --print(res:get_body_as_string(10))
+  end)
+  assert(cq:loop())
+end
+
+return M
