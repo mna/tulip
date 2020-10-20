79a080f3bdffaae733dffe7ed4579a5d7c74f826
Martin Angers <martin.n.angers@gmail.com>
2020-10-19 21:21:41 -0400

    Notes about cron package configuration

--- doc/example_config.lua
+++ doc/example_config.lua
@@ -73,6 +73,20 @@ local app = App{
   ['third.party.pkg'] = {
     -- config for that package
   },
+
+  cron = {
+             -- ┌───────────── min (0 - 59)
+             -- │ ┌────────────── hour (0 - 23)
+             -- │ │ ┌─────────────── day of month (1 - 31)
+             -- │ │ │ ┌──────────────── month (1 - 12)
+             -- │ │ │ │ ┌───────────────── day of week (0 - 6) (0 to 6 are Sunday to
+             -- │ │ │ │ │                  Saturday, or use names; 7 is also Sunday)
+             -- │ │ │ │ │
+             -- │ │ │ │ │
+    job_name = '* * * * *',
+
+    other_job = { schedule = '...', command = '...', max_attempts = 3, max_age = 30 },
+  },
 }
 
 assert(app:run())
