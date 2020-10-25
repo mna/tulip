a12e75765ab3fb8346916e299436a79d4f139a2a
Martin Angers <martin.n.angers@gmail.com>
2020-10-25 19:43:41 -0400

    Try to build pg_cron with postgres13 on fedora 32, no luck

--- scripts/cmds/deploy/userdata.lua
+++ scripts/cmds/deploy/userdata.lua
@@ -1,12 +1,20 @@
-return [[#!/usr/bin/env bash
+local function bash()
+  return [[#!/usr/bin/env bash
 
 # This is the cloud-init user data script used to setup a new machine
 # in a consistent way.
 
 set -euo pipefail
+]]
+end
 
+local function dnf()
+  return [[
 # upgrade all packages
 dnf --assumeyes upgrade
+
+# TODO: actually better use postgres12 as bundled in fedora, too much
+# trouble fixing unexpected paths and stuff with pg_cron
 
 # install the postgres 13 yum repository
 dnf --assumeyes install  \
@@ -18,31 +26,31 @@ dnf --assumeyes install  \
   fail2ban               \
   gcc                    \
   git                    \
+  libpq5-devel           \
   lsof                   \
   lua-devel              \
   luarocks               \
+  make                   \
   openssl-devel          \
   postgresql13-devel     \
   postgresql13-server    \
   sendmail               \
   the_silver_searcher    \
   vim
+]]
+end
 
-# TODO: generate secrets
-
+local function firewalld()
+  return [[
 # configure firewalld
 systemctl enable firewalld --now
 firewall-cmd --zone=public --add-service=http --add-service=https
 firewall-cmd --zone=public --add-service=http --add-service=https --permanent
+]]
+end
 
-# TODO: configure certbot
-
-# initialize the database and start the service
-/usr/pgsql-13/bin/postgresql-13-setup initdb
-systemctl enable --now postgresql-13
-# TODO: set postgres root password, update configuration as required
-# TODO: install, build and enable the pg_cron extension
-
+local function fail2ban()
+  return [[
 # configure fail2ban
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
 sed -i 's/^logtarget =.*/logtarget = sysout/g' /etc/fail2ban/fail2ban.local
@@ -63,9 +71,43 @@ systemctl enable sendmail
 systemctl enable fail2ban
 
 # TODO: custom log scanning for app abuse?
+]]
+end
 
+local function postgres()
+  return [[
+# build and install pg_cron
+pushd /tmp
+git clone https://github.com/citusdata/pg_cron.git
+cd pg_cron
+make && make install
+popd
+
+# initialize the database and start the service
+/usr/pgsql-13/bin/postgresql-13-setup initdb
+systemctl enable --now postgresql-13
+psql --username postgres --command 'CREATE EXTENSION pg_cron;'
+
+# TODO: set postgres root password, update configuration as required
+]]
+end
+
+local function luadeps()
+  return [[
 # install pre-required Lua dependencies (not handled by the rockspec
 # application file).
 luarocks install luarocks-fetch-gitrec
 luarocks install luaossl 'CFLAGS=-DHAVE_EVP_KDF_CTX=1 -fPIC'
 ]]
+end
+
+return table.concat{
+  bash(),
+  dnf(),
+  -- TODO: generate secrets
+  firewalld(),
+  -- TODO: configure certbot
+  fail2ban(),
+  postgres(),
+  luadeps(),
+}
