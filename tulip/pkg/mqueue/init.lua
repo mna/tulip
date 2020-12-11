c3c7f789fe384dcee17cd48d68a7226a34383075
Martin Angers <martin.n.angers@gmail.com>
2020-12-11 15:51:39 -0500

    Improve mqueue package doc

--- tulip/pkg/mqueue/init.lua
+++ tulip/pkg/mqueue/init.lua
@@ -49,9 +49,12 @@ local M = {
 
 -- The mqueue package registers an App:mqueue method that either
 -- enqueues a message, or dequeues a batch of messages to process.
+-- It implements a message queue with at-least-once delivery.
 --
--- Requires: a database package
+-- Requires: database package
+--
 -- Config:
+--
 --   * allowed_queues: array of string = if set, only those queues
 --     will be allowed.
 --   * default_max_age: integer|nil = if set, use as default max age
@@ -59,7 +62,12 @@ local M = {
 --   * default_max_attempts: integer|nil = if set, use as default
 --     maximum number of attempts to process a message.
 --
+-- Methods:
+--
 -- v, err = App:mqueue(t[, conn[, msg]])
+--
+--   Enqueues a message, or dequeues a batch of messages to process.
+--
 --   > t: table = a table with the following fields:
 --     * t.max_attempts: number|nil = maximum number of attempts (enqueue only)
 --     * t.max_age: number|nil = number of seconds to process message (enqueue only)
@@ -73,7 +81,50 @@ local M = {
 --     that indicates if the message was enqueued, otherwise returns an
 --     array of tables representing the messages to process. Each table has
 --     an id and a payload fields and a :done(conn) method. Is nil on error.
---   < err: string|nil = error message if v is nil.
+--   < err: Error|nil = error message if v is nil
+--
+-- res, err = Message:done(conn)
+--
+--   Marks the message as done so it isn't processed anymore. Note that if
+--   the max age of the message was expired, the message would still be
+--   pending again after the call.
+--
+--   > conn: connection = the database connection to use
+--   < res: userdata = the database result
+--   < err: Error|nil = error message if res is nil
+--
+-- Message.raw_payload: string
+--
+--   The JSON payload as a string, as stored in the DB.
+--
+-- Message.payload_err: string
+--
+--   The JSON decoding error, if any.
+--
+-- Message.payload: table
+--
+--   The payload as a Lua table, JSON-decoded.
+--
+-- Message.id: number
+--
+--   The id of the message.
+--
+-- Message.attempts: number
+--
+--   The number of attempts done so far (not including the current one).
+--
+-- Message.max_attempts: number
+--
+--   The maximum number of attempts for this message.
+--
+-- Message.max_age: number
+--
+--   The max age of this message (time allocated for processing).
+--
+-- Message.first_created: epoch
+--
+--   The timestamp when the message was first created, as epoch value.
+--
 function M.register(cfg, app)
   tcheck({'table', 'tulip.App'}, cfg, app)
   app.mqueue = make_mqueue(cfg)
