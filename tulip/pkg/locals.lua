d6b0e25bfc7396dac4c23f0ff060d203dafae2ce
Martin Angers <martin.n.angers@gmail.com>
2020-12-11 14:12:45 -0500

    Improve locals package doc

--- tulip/pkg/locals.lua
+++ tulip/pkg/locals.lua
@@ -3,8 +3,19 @@ local tcheck = require 'tcheck'
 local M = {}
 
 -- The locals package adds a key-value dictionary on the app
--- under the 'locals' field. This can be used to set app-wide
--- information such as name, title, contact email address, etc.
+-- under the 'locals' field.
+--
+-- Config:
+--
+--  * arbitrary key-value.
+--
+-- Fields:
+--
+-- * App.locals: table
+--
+--   Key-value dictionary initialized with the configuration table. This can be
+--   used to set app-wide information such as name, title, contact email address,
+--   etc.
 function M.register(cfg, app)
   tcheck({'table', 'tulip.App'}, cfg, app)
   app.locals = cfg
