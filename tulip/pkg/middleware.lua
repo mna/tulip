77c5a22d028d556e7d1c5951d307dbed8518ec1f
Martin Angers <martin.n.angers@gmail.com>
2020-12-11 15:22:10 -0500

    Improve middleware package doc

--- tulip/pkg/middleware.lua
+++ tulip/pkg/middleware.lua
@@ -28,11 +28,53 @@ local M = {
   },
 }
 
--- The middleware package enables app-level middleware in the
+-- The middleware package enables app-level middleware (as opposed
+-- to route-specific middleware) in the
 -- order specified in the configuration. This means that requests
 -- will go through those middleware handlers in that order.
--- It also sets up the __call metamethod on the App's metatable so
--- that it can be used as initial middleware.
+--
+-- Config: array of string|function = the order of the middleware
+-- to apply to web requests.
+--
+-- Methods and Fields:
+--
+-- App(req, res, nxt)
+--
+--   Sets up the __call metamethod on the App's metatable so
+--   that it can be used as initial middleware.
+--
+--   > req: table = the http Request instance
+--   > res: table = the http Response instance
+--   > nxt: function|nil = the function that calls the next middleware
+--
+-- v = App:lookup_middleware(name)
+--
+--   Returns the middleware registered for that name, or nil if none.
+--
+--   > name: string = the name for the registered middleware
+--   < v: array = the registered middleware
+--
+-- App:register_middleware(name, mw)
+--
+--   Registers the middleware mw for name. If there is already a registered
+--   middleware for name, throws an error.
+--
+--   > name: string = the name of the middleware
+--   > mw: function = middleware function (or callable table)
+--
+-- App:resolve_middleware(mws)
+--
+--   Resolves the middleware identified by a string in the array mws to
+--   their actual function (or callable table).
+--
+--   > mws: array of string|function = the defined middleware
+--
+-- App.middleware: array of string|function
+--
+--   The list of middleware to apply to web requests. When the App is
+--   called as initial middleware, it triggers that chain of middleware
+--   next, in sequence.
+--
 function M.register(cfg, app)
   tcheck({'table', 'tulip.App'}, cfg, app)
   app.middleware = cfg
