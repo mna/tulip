bd33837e722c89f1efbd8e202e5009d4457ee6f2
Martin Angers <martin.n.angers@gmail.com>
2020-12-11 22:21:56 -0500

    Improve reqid package doc

--- tulip/pkg/reqid.lua
+++ tulip/pkg/reqid.lua
@@ -23,9 +23,23 @@ local M = {
 }
 
 -- The reqid package registers a middleware that adds a unique
--- id to each request. The id is added to both the request and the
--- response's headers (under the header name provided in the configuration)
--- and is also added on the request.locals.request_id field.
+-- id to each request.
+--
+-- Requires: the middleware package
+--
+-- Config:
+--
+-- * size: integer = size of the raw ID, before base-64 encoding (Default: 12)
+-- * header: string = name of the header where to set the ID (Default: 'x-request-id')
+--
+-- Middleware:
+--
+-- * tulip.pkg.reqid
+--
+--   Adds a unique id to each request. The id is added to both the request and the
+--   response's headers (under the header name provided in the configuration)
+--   and is also added on the request.locals.request_id field.
+--
 function M.register(cfg, app)
   tcheck({'table', 'tulip.App'}, cfg, app)
   app:register_middleware('tulip.pkg.reqid', make_middleware(cfg))
